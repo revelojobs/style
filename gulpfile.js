45b54fc870b539b5ab53f3efc2d1a0fb8ca5ee74
Felipe Orlando <fobsouza@gmail.com>
2018-06-18 09:51:01 -0300

    Add gulp configs, default scripts, and stylemark yml

--- gulpfile.js
+++ gulpfile.js
@@ -1,68 +1,51 @@
-const gulp = require('gulp');
-const svgmin = require('gulp-svgmin');
-const sassInlineSvg = require('gulp-sass-inline-svg');
-const sass = require('gulp-sass');
-const cleanCSS = require('gulp-clean-css');
-const autoprefixer = require('gulp-autoprefixer');
-const liveReload = require('gulp-livereload');
-const browserSync = require('browser-sync');
-const runSequence = require('run-sequence');
+const gulp = require('gulp')
+const svgmin = require('gulp-svgmin')
+const sassInlineSvg = require('gulp-sass-inline-svg')
+const sass = require('gulp-sass')
+const cleanCSS = require('gulp-clean-css')
+const autoprefixer = require('gulp-autoprefixer')
+const rename = require('gulp-rename')
+const runSequence = require('run-sequence')
 
 gulp.task('svg', function(){
-  return gulp.src('./src/icons/**/*.svg') 
+  return gulp.src('./src/icons/**/*.svg')
     .pipe(svgmin())
     .pipe(sassInlineSvg({
       destDir: './src/stylesheets/vendor'
-    }));
-});
+    }))
+})
 
 gulp.task('styles', () => {
   return gulp.src('./src/stylesheets/main.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
-    .pipe(gulp.dest('./dist/stylesheets'));
-});
-
-gulp.task('minify-styles', () => {
-  return gulp.src('./dist/stylesheets/main.css')
+    .pipe(gulp.dest('./dist/stylesheets'))
+    .pipe(gulp.dest('./docs/assets'))
     .pipe(cleanCSS())
-    .pipe(gulp.dest('./dist/stylesheets'));
-});
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./dist/stylesheets'))
+})
 
 gulp.task('style-helpers', () => {
-  const paths = ['functions', 'variables'];
-  const base  = './src/stylesheets/';
-  const dist  = './dist/stylesheets';
+  const paths = ['functions', 'variables']
+  const base  = './src/stylesheets/'
+  const dist  = './dist/stylesheets'
 
   return paths.forEach((path) => {
-    let src = base + path + '/**/*';
-    gulp.src(src, { base: base }).pipe(gulp.dest(dist));
-  });
+    const src = base + path + '/**/*'
+    gulp.src(src, { base: base }).pipe(gulp.dest(dist))
+  })
+})
+
+gulp.task('watch', () => {
+  gulp.watch('./src/stylesheets/**/*', ['svg', 'styles']);
 });
-
-// gulp.task('watch', () => {
-//   liveReload.listen();
-
-//   gulp.watch('./src/stylesheets/**/*', ['styles']);
-// });
-
-// gulp.task('serve', ['styles'], () => {
-//   const browserSyncInstance = browserSync.create();
-
-//   browserSyncInstance.init({
-//     port: 8000,
-//     server: {
-//       baseDir: './dist'
-//     }
-//   });
-
-//   gulp.watch('./dist/**/*').on('change', browserSyncInstance.reload);
-// });
-
-// gulp.task('development', () => {
-//   runSequence('svg', 'styles', 'images', 'serve', 'watch');
-// });
 
 gulp.task('default', () => {
-  runSequence('svg', 'styles', 'minify-styles', 'style-helpers');
-});
+  runSequence(
+    'svg',
+    'styles',
+    'style-helpers',
+    'watch'
+  )
+})
